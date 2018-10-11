02c2279e570568eaa615b4e672ec9ad74b59b114
Mikhail <ponomarev@interneturok.ru>
2018-10-11 12:51:36 +0500

    POM\popup_auth_and_reg.py
    Написал page object model

---
+++ POM/popup_auth_and_reg.py
@@ -0,0 +1,48 @@
+import time
+
+
+class PopupSignIn(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def close_popup(self):
+        self.driver.find_element_by_class_name("m-close").click()
+
+    def go_to_popup_registration(self):
+        self.driver.find_element_by_link_text("Регистрация").click()
+
+    def go_to_popup_authorization(self):
+        self.driver.find_element_by_link_text("Вход").click()
+
+    def click_button_vk_reg(self):
+        self.driver.find_element_by_xpath("//*[@id='rec59637330']/div/div/div[2]/div/div[1]/ul/li[1]/a").click()
+        assert (u"Я новый пользователь",
+                self.driver.find_element_by_css_selector("body > div > div.actions > a:nth-child(1)").text)
+
+    def click_button_vk_auth(self):
+        self.driver.find_element_by_xpath("//*[@id='rec59637329']/div/div/div[2]/div/div[1]/ul/li[1]/a").click()
+        assert (self.driver.find_element_by_css_selector("div.header__menu.header__menu_profile"))
+
+    def click_button_odnoklassniki(self):
+        self.driver.find_element_by_css_selector("a.b-omniauth__item_icon_od").click()
+
+    def click_button_mail(self):
+        self.driver.find_element_by_css_selector("a.b-omniauth__item.b-omniauth__item_icon_mm").click()
+
+    def click_button_facebook(self):
+        self.driver.find_element_by_css_selector("b-omniauth__item.b-omniauth__item_icon_fb").click()
+
+    def click_button_google(self):
+        self.driver.find_element_by_css_selector("a.b-omniauth__item.b-omniauth__item_icon_gplus").click()
+
+    def click_button_twitter(self):
+        self.driver.find_element_by_css_selector("a.b-omniauth__item.b-omniauth__item_icon_tw").click()
+
+    def click_button_yandex(self):
+        self.driver.find_element_by_css_selector("a.b-omniauth__item.b-omniauth__item_icon_yd").click()
+
+    def click_button_login(self):
+        self.driver.find_element_by_css_selector(
+            "#form59637329 > div.t330__input-wrapper > div.t330__blockbutton > button").click()
+        assert (self.driver.find_element_by_css_selector("a.dropdown-toggle"))
+        time.sleep(2)
