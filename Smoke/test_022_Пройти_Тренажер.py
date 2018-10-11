dcb5c94b320ef19df2137d285b3103ea345e607a
Mikhail <ponomarev@interneturok.ru>
2018-10-11 12:45:50 +0500

    Smoke\test_022_Пройти_Тренажер.p
    Написал тест

---
+++ Smoke/test_022_Пройти_Тренажер.py
@@ -0,0 +1,41 @@
+import allure
+from POM.popup_auth_and_reg import PopupSignIn
+from POM.setup import StartTildaClassMethod
+from POM.tilda_page import TildaPage
+from POM.users import Hs05
+from POM.url import UrlHomeSchool
+from POM.lesson_page import LessonPage
+from POM.TT import Exercise
+from POM.asserts import AssertForTest022
+
+
+@allure.feature("Тренажер")
+@allure.story("Авторизоваться, Пройти Тренажер")
+class PassTrainer(StartTildaClassMethod):
+    def test_trainer_passed(self):
+        driver = self.driver
+        step_tilda = TildaPage(driver)
+        step_enter = PopupSignIn(driver)
+        step_user = Hs05(driver)
+        url_go = UrlHomeSchool(driver)
+        steps_test = LessonPage(driver)
+        ask_trainer = Exercise(driver)
+        assert_step = AssertForTest022(driver)
+
+        with allure.step("На TILDA нажать на кнопку Войти"):
+            step_tilda.click_button_enter()
+        with allure.step("В поле email и password ввести hs02@yopmail.com/123456"):
+            step_user.enter_email(user_name="hs02@yopmail.com")
+            step_user.enter_password(password="123456")
+        with allure.step("Нажать на кнопку Авторизоваться"):
+            step_enter.click_button_login()
+        with allure.step("Перейти на урок"):
+            url_go.go_to_lesson_tab_trainer_iu()
+        with allure.step("В Тренажере нажать на кнопку Пройти"):
+            steps_test.go_trainer()
+        with allure.step("Отвечать на вопросы в Тренажере"):
+            ask_trainer.test()
+        with allure.step("В финальном поп-апе нажать на кнопку Завершить"):
+            ask_trainer.click_button_finish()
+        with allure.step("После пройденного Теста название кнопки Пройти поменялась на Повторить"):
+            assert_step.check_button_finish_trainer()
