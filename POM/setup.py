9c3786625efd00606e67ab8c28d5fcd572183fbc
Mikhail <ponomarev@interneturok.ru>
2018-10-15 23:05:16 +0500

    POM\setup.p
    Добавил сетап для АМО

--- POM/setup.py
+++ POM/setup.py
@@ -3,6 +3,7 @@ import unittest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+
 class StarSchool(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -116,6 +117,34 @@ class StartLandingClassMethod(unittest.TestCase):
         cls.driver.implicitly_wait(40)
         cls.driver.maximize_window()
         cls.driver.get("https://web-dev01.interneturok.ru/school_landing/")
+        time.sleep(3)
+        cls.verificationErrors = []
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+class StartAmoClassMethod(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(40)
+        cls.driver.maximize_window()
+        cls.driver.get("https://www.amocrm.ru/")
         time.sleep(3)
         cls.verificationErrors = []
 
