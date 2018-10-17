ff120f1a2bc8d8d8110c7757f3a9d0b3145acf63
Mikhail <ponomarev@interneturok.ru>
2018-10-17 11:29:51 +0500

    Update POM/url.py
    Поменял индификатор явного ожидания

--- POM/url.py
+++ POM/url.py
@@ -7,8 +7,7 @@ class UrlHomeSchool(object):
 
     def go_to_my_profile(self):
         self.driver.get("https://web-dev01.interneturok.ru/school/profile/my_profile")
-        assert (
-            "Моя школа:", self.driver.find_element_by_class_name("profile-courses_item_header").text)
+        assert (self.driver.find_element_by_css_selector("button.btn.btn-primary.btn-centered"))
 
     def go_to_tilda_landing(self):
         self.driver.get("http://iu:123456@project752209.tilda.ws/")
@@ -20,7 +19,7 @@ class UrlHomeSchool(object):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21715/video/92701")
 
     def go_to_lesson_page_tab_homework(self):
-            self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21715/homework/92702")
+        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21715/homework/92702")
 
     def go_to_lesson_tab_test_iu(self):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/18402/training_iu/91896")
@@ -36,4 +35,3 @@ class UrlHomeSchool(object):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/18112/video/72658")
         assert (self.driver.find_element_by_css_selector("#step9 > button"))
         time.sleep(1.5)
-
