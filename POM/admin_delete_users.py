c3805094ce9bc111a59c00e4b974b1abe5287e43
Mikhail <ponomarev@interneturok.ru>
2018-10-22 13:41:44 +0500

    Update POM/admin_delete_users.py
    Поменял локаторы и добавил время в time.sleep

--- POM/admin_delete_users.py
+++ POM/admin_delete_users.py
@@ -15,11 +15,11 @@ class AdminDeleteUser(object):
         self.driver.find_element_by_css_selector("input.form-control.ng-pristine.ng-untouched.ng-valid").send_keys(
             pupil_1)
         self.driver.find_element_by_css_selector(".input-group-btn > button:nth-child(1)").click()
-        time.sleep(3)
         assert (self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-pencil"))
+        time.sleep(3)
         self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-remove").click()
         time.sleep(1)
-        self.driver.find_element_by_css_selector(".modal-footer > button:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("html.no-js.ng-scope body.modal-open div.modal.fade.ng-isolate-scope.iu-fade.in div.modal-dialog div.modal-content div.ng-scope div.modal-footer.ng-scope button.btn.btn-primary").click()
         time.sleep(1)
         self.driver.find_element_by_css_selector("button.btn-success:nth-child(1)").click()
         time.sleep(2)
@@ -28,16 +28,15 @@ class AdminDeleteUser(object):
         time.sleep(2)
 
     def user_2(self, pupil_2):
-        self.driver.find_element_by_css_selector("input.form-control.ng-pristine.ng-untouched.ng-valid").clear()
+        self.driver.find_element_by_css_selector("input.form-control").clear()
         self.driver.find_element_by_css_selector("input.form-control").send_keys(
             pupil_2)
-        # assert (self.driver.find_element_by_css_selector("button.btn:nth-child(3)"))
         self.driver.find_element_by_css_selector(".input-group-btn > button:nth-child(1)").click()
         assert (self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-pencil"))
         time.sleep(3)
         self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-remove").click()
         time.sleep(1)
-        self.driver.find_element_by_css_selector(".modal-footer > button:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("html.no-js.ng-scope body.modal-open div.modal.fade.ng-isolate-scope.iu-fade.in div.modal-dialog div.modal-content div.ng-scope div.modal-footer.ng-scope button.btn.btn-primary").click()
         time.sleep(1)
         self.driver.find_element_by_css_selector("button.btn-success:nth-child(1)").click()
         time.sleep(2)
@@ -46,16 +45,15 @@ class AdminDeleteUser(object):
         time.sleep(2)
 
     def user_3(self, pupil_3):
-        self.driver.find_element_by_css_selector("input.form-control.ng-pristine.ng-untouched.ng-valid").clear()
+        self.driver.find_element_by_css_selector("input.form-control").clear()
         self.driver.find_element_by_css_selector("input.form-control").send_keys(
             pupil_3)
-        # assert (self.driver.find_element_by_css_selector("button.btn:nth-child(3)"))
         self.driver.find_element_by_css_selector(".input-group-btn > button:nth-child(1)").click()
         assert (self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-pencil"))
         time.sleep(3)
         self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-remove").click()
         time.sleep(1)
-        self.driver.find_element_by_css_selector(".modal-footer > button:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("html.no-js.ng-scope body.modal-open div.modal.fade.ng-isolate-scope.iu-fade.in div.modal-dialog div.modal-content div.ng-scope div.modal-footer.ng-scope button.btn.btn-primary").click()
         time.sleep(1)
         self.driver.find_element_by_css_selector("button.btn-success:nth-child(1)").click()
         time.sleep(2)
@@ -64,16 +62,15 @@ class AdminDeleteUser(object):
         time.sleep(2)
 
     def user_4(self, pupil_4):
-        self.driver.find_element_by_css_selector("input.form-control.ng-pristine.ng-untouched.ng-valid").clear()
+        self.driver.find_element_by_css_selector("input.form-control").clear()
         self.driver.find_element_by_css_selector("input.form-control").send_keys(
             pupil_4)
-        # assert (self.driver.find_element_by_css_selector("button.btn:nth-child(3)"))
         self.driver.find_element_by_css_selector(".input-group-btn > button:nth-child(1)").click()
         assert (self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-pencil"))
         time.sleep(3)
         self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-remove").click()
         time.sleep(1)
-        self.driver.find_element_by_css_selector(".modal-footer > button:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("html.no-js.ng-scope body.modal-open div.modal.fade.ng-isolate-scope.iu-fade.in div.modal-dialog div.modal-content div.ng-scope div.modal-footer.ng-scope button.btn.btn-primary").click()
         time.sleep(1)
         self.driver.find_element_by_css_selector("button.btn-success:nth-child(1)").click()
         time.sleep(2)
@@ -82,16 +79,15 @@ class AdminDeleteUser(object):
         time.sleep(2)
 
     def user_5(self, pupil_5):
-        self.driver.find_element_by_css_selector("input.form-control.ng-pristine.ng-untouched.ng-valid").clear()
+        self.driver.find_element_by_css_selector("input.form-control").clear()
         self.driver.find_element_by_css_selector("input.form-control").send_keys(
             pupil_5)
-        # assert (self.driver.find_element_by_css_selector("button.btn:nth-child(3)"))
         self.driver.find_element_by_css_selector(".input-group-btn > button:nth-child(1)").click()
         assert (self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-pencil"))
-        time.sleep(3)
+        time.sleep(5)
         self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-remove").click()
         time.sleep(1)
-        self.driver.find_element_by_css_selector(".modal-footer > button:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("html.no-js.ng-scope body.modal-open div.modal.fade.ng-isolate-scope.iu-fade.in div.modal-dialog div.modal-content div.ng-scope div.modal-footer.ng-scope button.btn.btn-primary").click()
         time.sleep(1)
         self.driver.find_element_by_css_selector("button.btn-success:nth-child(1)").click()
         time.sleep(2)
@@ -100,16 +96,15 @@ class AdminDeleteUser(object):
         time.sleep(2)
 
     def user_6(self, pupil_6):
-        self.driver.find_element_by_css_selector("input.form-control.ng-pristine.ng-untouched.ng-valid").clear()
+        self.driver.find_element_by_css_selector("input.form-control").clear()
         self.driver.find_element_by_css_selector("input.form-control").send_keys(
             pupil_6)
-        # assert (self.driver.find_element_by_css_selector("button.btn:nth-child(3)"))
         self.driver.find_element_by_css_selector(".input-group-btn > button:nth-child(1)").click()
         assert (self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-pencil"))
-        time.sleep(3)
+        time.sleep(5)
         self.driver.find_element_by_css_selector("i.glyphicon.glyphicon-remove").click()
         time.sleep(1)
-        self.driver.find_element_by_css_selector(".modal-footer > button:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("html.no-js.ng-scope body.modal-open div.modal.fade.ng-isolate-scope.iu-fade.in div.modal-dialog div.modal-content div.ng-scope div.modal-footer.ng-scope button.btn.btn-primary").click()
         time.sleep(1)
         self.driver.find_element_by_css_selector("button.btn-success:nth-child(1)").click()
         time.sleep(2)
