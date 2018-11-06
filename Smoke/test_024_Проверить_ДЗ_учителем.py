3b7d6dfcfcb532fd8857fe9b677941b3853f4e54
Mikhail <ponomarev@interneturok.ru>
2018-11-06 17:17:36 +0500

    Update Smoke/test_024_Проверить_ДЗ_учителем.py
    Дописал проверку оценки на странцие урока у ученика

--- Smoke/test_024_Проверить_ДЗ_учителем.py
+++ Smoke/test_024_Проверить_ДЗ_учителем.py
@@ -38,6 +38,7 @@ class LoginTeacherAndCheckHomeWorks(StartSchoolClassMethod):
             step_teacher.click_button_save()
         with allure.step("После проставления оценки отображается оценка 4 и ФИО учителя"):
             step_assert.check_bal_and_teacher()
+            driver.delete_all_cookies()
 
     def test_check_ball_for_student(self):
         driver = self.driver
@@ -45,8 +46,9 @@ class LoginTeacherAndCheckHomeWorks(StartSchoolClassMethod):
         step_enter = PopupSignIn(driver)
         step_user = Hs05(driver)
         step_url = UrlHomeSchool(driver)
+        step_assert = AssertForTest024(driver)
 
-        with allure.step("Перейти на страницу урока"):
+        with allure.step("Перейти на Tilda"):
             step_url.go_to_tilda_landing()
         with allure.step("На TILDA нажать на кнопку Войти"):
             step_tilda.click_button_enter()
@@ -57,3 +59,5 @@ class LoginTeacherAndCheckHomeWorks(StartSchoolClassMethod):
             step_enter.click_button_login()
         with allure.step("Перейти на страницу урока"):
             step_url.go_to_lesson_page_tab_homework()
+        with allure.step("Проверить на странице урока во вкладке ДЗ отображение оценки Итоговая оценка: 4 / Хорошо "):
+            step_assert.check_bal_in_homweork_for_lesson_page()
