c53735de8777ff397e76b5e66257216e8d3b9f5f
Mikhail <ponomarev@interneturok.ru>
2018-11-15 11:05:34 +0500

    Update POM/url.py
    Добавил новую ссылку на урок + редактировал старые ссылки

--- POM/url.py
+++ POM/url.py
@@ -17,10 +17,13 @@ class UrlHomeSchool(object):
         self.driver.get("https://web-dev01.interneturok.ru/school/logout")
 
     def go_to_lesson_page_test(self):
-        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21715/video/92701")
+        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21806/video/94578")
+
+    def go_to_lesson_page_1_klass(self):
+        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21519/homework/90734")
 
     def go_to_lesson_page_tab_homework(self):
-        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21715/homework/92702")
+        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21806/homework/94579")
         assert (self.driver.find_element_by_css_selector(
             "body > div.page-wrapper.ng-isolate-scope > div > div.container > div.row.ng-scope > div > div > div:nth-child(2) > div"))
         time.sleep(6)
