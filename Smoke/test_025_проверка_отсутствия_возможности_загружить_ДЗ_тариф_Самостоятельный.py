c082b4ad44d7f2f1e3904408eb15c188b37dac4e
Mikhail <ponomarev@interneturok.ru>
2018-12-11 15:09:44 +0500

    Создал два кейса в smoke
    1. 003_регистрация_покупка_10_класс_Сзачислением_9месяцев_и_ПН_с_вкл_АП
    2. 025_проверка_отсутствия_возможности_загружить_ДЗ_тариф_Самостоятельный

---
+++ Smoke/test_025_проверка_отсутствия_возможности_загружить_ДЗ_тариф_Самостоятельный.py
@@ -0,0 +1,38 @@
+import allure
+from POM.setup import StartTildaClassMethod
+from POM.tilda_page import TildaPage
+from POM.popup_auth_and_reg import PopupSignIn
+from POM.users import Hs01
+from POM.asserts import AssertForTest025
+from POM.url import UrlHomeSchool
+
+
+@allure.feature("Тариф Самостоятельный")
+@allure.story("Проверка отсутствия загрузки ДЗ и заглушка на Яклассе")
+class LoginAndGoToLessonPageCheckHomeworkAndYaklass(StartTildaClassMethod):
+    def test_000_go_lesson_page(self):
+        driver = self.driver
+        step_tilda = TildaPage(driver)
+        step_enter = PopupSignIn(driver)
+        step_user = Hs01(driver)
+
+        with allure.step("На TILDA нажать на кнопку Войти"):
+            step_tilda.click_button_enter()
+        with allure.step("В поле email и password ввести hs01@yopmail.com/123456"):
+            step_user.enter_email(user_name="hs01@yopmail.com")
+            step_user.enter_password(password="123456")
+        with allure.step("Нажать на кнопку Авторизоваться"):
+            step_enter.click_button_login()
+
+    def test_001_failing_download_homework(self):
+        driver = self.driver
+        go_lesson_page = UrlHomeSchool(driver)
+        step_assert = AssertForTest025(driver)
+        with allure.step(
+                "Перейти на страницу урока Русский язык, 1 класс , неделя 14 (26 ноября - 1 декабря)Строчная и заглавная буквы Д, д"):
+            go_lesson_page.go_to_lesson_page_1_klass()
+        with allure.step("В шаге № 1 Якласса отображается заглушка с текстом Доступ к материалам ограничен"):
+            step_assert.check_step_one()
+        with allure.step(
+                "В шаге №2 отображается заглушка В формате обучения «С учителем» вы сможете загрузить свое решение."):
+            step_assert.check_step_two()
