367064414fd98c78169e2d5857d1c07d741daaf8
Mikhail <ponomarev@interneturok.ru>
2019-01-18 16:18:53 +0500

    Файл test_adminka_API.py
    Добавил импорт requests и прописал новые классы в init

--- test_adminka_API.py
+++ test_adminka_API.py
@@ -1,6 +1,7 @@
+import requests
 import unittest
 from Unit_tests.test_admin_panel import TestFilterSubjects
 
-suite = unittest.TestSuite()
+suite = requests.TestSuite()
 
-suite.addTest(unittest.makeSuite(TestFilterSubjects))
+suite.addTest(requests.makeSuite(TestFilterSubjects))
