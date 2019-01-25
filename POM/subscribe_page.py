443fc86053ca4676243af496111ee6326160ad75
Mikhail <ponomarev@interneturok.ru>
2019-01-25 23:54:40 +0500

    Файл POM subscribe_page.py
    Поменял локаторы с xpath на css

--- POM/subscribe_page.py
+++ POM/subscribe_page.py
@@ -6,77 +6,68 @@ class PageSubscribe(object):
         self.driver = driver
 
     def select_one_class(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]").click()
+        self.driver.find_element_by_css_selector(".payment-class-item:nth-child(1)").click()
 
     def select_six_class(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[7]").click()
+        self.driver.find_element_by_css_selector(".payment-class-item:nth-child(6)").click()
 
-    def select_eleven_class(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[10]").click()
+    def select_ten_class(self):
+        self.driver.find_element_by_css_selector(".payment-class-item:nth-child(10)").click()
         time.sleep(0.5)
 
     def choose_tariff_independent(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]").click()
+        self.driver.find_element_by_css_selector(".payment-format-item:nth-child(1)").click()
 
     def choose_tariff_with_the_teacher(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[2]/div[1]").click()
+        self.driver.find_element_by_css_selector(".payment-format-item:nth-child(2)").click()
 
     def choose_tariff_with_enrollment(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[3]/div[1]").click()
+        self.driver.find_element_by_css_selector(".payment-format-item:nth-child(3)").click()
 
     def choose_period_one_month(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[3]/div[2]/div/div[1]").click()
+        self.driver.find_element_by_css_selector(".payment-period-item:nth-child(1)").click()
 
     def choose_period_three_month(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[3]/div[2]/div/div[2]").click()
+        self.driver.find_element_by_css_selector(".payment-period-item:nth-child(2)").click()
 
     def choose_period_nine_month(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[3]/div[2]/div/div[3]").click()
+        self.driver.find_element_by_css_selector(".payment-period-item:nth-child(3)").click()
 
     def selected_service_personal_mentor(self):
         self.driver.find_element_by_css_selector("label.mentor__checkbox-label").click()
         time.sleep(1)
 
     def extend_service_personal_mentor(self):
-        self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div[5]/div[1]/div[1]/label").click()
+        self.driver.find_element_by_css_selector(".mentor__checkbox-label").click()
         time.sleep(1)
 
     def click_off_button_auto_payments_in_curse(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[3]/div[3]/label").click()
+        self.driver.find_element_by_css_selector(
+            "div:nth-child(3) > div.payment-item_content_autopay.ng-scope > label").click()
 
     def enable_button_auto_payments_in_personal_mentor(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[4]/div[3]/label").click()
+        self.driver.find_element_by_css_selector(
+            "div:nth-child(4) > div.payment-item_content_autopay.ng-scope > label").click()
 
     def click_button_see_tab_one_ege_curse(self):
         # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         self.driver.find_element_by_css_selector(
-            "body > div.page-wrapper.ng-isolate-scope > div > div.container > div.row.ng-scope > div > div.home-school.col-sm-12.no-padding.ng-scope > div:nth-child(2) > div > div > div:nth-child(2) > tarif-table-ege > div > button").click()
+            "div:nth-child(2) > tarif-table-ege > div > button").click()
         time.sleep(2)
 
     def choose_period_one_month_in_ege(self):
         self.driver.find_element_by_css_selector(
-            "body > div.page-wrapper.ng-isolate-scope > div > div.container > div.row.ng-scope > div > div.home-school.col-sm-12.no-padding.ng-scope > div:nth-child(2) > div > div > div:nth-child(2) > tarif-table-ege > div > div > div > table > tfoot > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1) > label > i").click()
+            "div:nth-child(2) > tarif-table-ege > div > div > div > table > tfoot > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1) > label > i").click()
         time.sleep(1)
 
     def choose_period_nine_month_in_ege(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[2]/div/div/div[1]/tarif-table-ege/div/div/div/table/tfoot/tr[1]/td[3]/div[2]/div[4]").click()
+        self.driver.find_element_by_css_selector(
+            "div:nth-child(2) > tarif-table-ege > div > div > div > table > tfoot > tr:nth-child(1) > td:nth-child(2) > div:nth-child(4) > label > i").click()
         time.sleep(1)
 
     def click_off_button_auto_payments_in_ege(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[2]/div/div/div[1]/tarif-table-ege/div/div/div/table/tfoot/tr[3]/td[2]/div/label").click()
+        self.driver.find_element_by_css_selector(
+            "div:nth-child(2) > tarif-table-ege > div > div > div > table > tfoot > tr:nth-child(3) > td:nth-child(2) > div > label").click()
         time.sleep(1)
 
     def click_button_pay_school(self):
@@ -88,8 +79,8 @@ class PageSubscribe(object):
         # time.sleep(40)
 
     def click_button_pay_ege_online(self):
-        self.driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div[2]/div/div/div[1]/tarif-table-ege/div/div/div/table/tfoot/tr[2]/td[3]/button").click()
+        self.driver.find_element_by_css_selector(
+            "div:nth-child(2) > div > div > div:nth-child(2) > tarif-table-ege > div > div > div > table > tfoot > tr:nth-child(2) > td:nth-child(3)").click()
         # time.sleep(40)
 
     def enter_data_card(self):
