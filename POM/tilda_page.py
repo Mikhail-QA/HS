222533a49410044f8dcc52dbae469d583750c568
Mikhail <ponomarev@interneturok.ru>
2019-04-23 10:18:57 +0500

    Директория POM, файл tilda_page.py
    Добавил декоратор в методы allure

--- POM/tilda_page.py
+++ POM/tilda_page.py
@@ -1,13 +1,20 @@
 import time
+import allure
 
 
 class TildaPage(object):
     def __init__(self, driver):
         self.driver = driver
 
-    def click_button_enter(self):
+    @allure.step
+    def open_tilda_page(self):
+        self.driver.get("https://iu:123456@test-school01.interneturok.ru/")
+
+    @allure.step
+    def click_login_button(self):
         self.driver.find_element_by_link_text("Войти").click()
 
+    @allure.step
     def click_button_registration(self):
         self.driver.find_element_by_link_text("Регистрация").click()
         time.sleep(1)
