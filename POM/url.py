33d6c5b9bc062db5bd4caaae56c1d7dac7ce3d2e
Mikhail <ponomarev@interneturok.ru>
2019-05-13 12:24:03 +0500

    Директория POM, файл url.py поменял ссылку на урок в go_to_lesson_page_test

--- POM/url.py
+++ POM/url.py
@@ -22,31 +22,33 @@ class UrlHomeSchool(object):
         self.driver.get("https://web-dev01.interneturok.ru/school/logout")
 
     def go_to_lesson_page_test(self):
-        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21860/video/95238")
+        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21877/video/95854")
         assert (self.driver.find_element_by_css_selector("#step9"))
 
     def go_to_lesson_page_1_klass(self):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21493/homework/91341")
 
     def go_to_lesson_page_tab_homework(self):
-        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21860/homework/95239")
+        self.driver.get("https://web-dev01.interneturok.ru/school/lesson/21877/homework/95855")
         assert (self.driver.find_element_by_css_selector(
             ".next-item-btn"))
         time.sleep(2)
 
     def go_to_lesson_tab_test_iu(self):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/18402/training_iu/91896")
-        assert (self.driver.find_element_by_css_selector("#step9 > button"))
+        assert (self.driver.find_element_by_css_selector(
+            ".training-iu-section:nth-child(2) div:nth-child(1) label span.ng-binding"))
         time.sleep(1.5)
 
     def go_to_lesson_tab_trainer_iu(self):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/18325/training_iu/90787")
-        assert (self.driver.find_element_by_css_selector("#step9 > button"))
+        assert (self.driver.find_element_by_css_selector(
+            ".training-iu-section:nth-child(1) div:nth-child(1) label span.ng-binding"))
         time.sleep(1.5)
 
     def go_to_lesson_tab_video(self):
         self.driver.get("https://web-dev01.interneturok.ru/school/lesson/18112/video/72658")
-        assert (self.driver.find_element_by_css_selector("#step9 > button"))
+        assert (self.driver.find_element_by_id("vlp-videobox-1"))
         time.sleep(1.5)
 
     def go_to_page_subjects_subscribe(self):
